1083ef1462fe05d467ae8e850435cb5f90f4f6f0
jerrymiahdg <kingbebop15@gmail.com>
2023-11-26 08:43:49 -0800

    hopefully fixes video for mobile

--- all-about-green-energy-lakshya/src/components/pages/home/home.js
+++ all-about-green-energy-lakshya/src/components/pages/home/home.js
@@ -23,7 +23,8 @@ const Home = () => {
         "Basically, green energy is energy that is produced without harming the natural environment.",
       ],
       img: "https://dalrada.com/wp-content/uploads/2022/07/clean-vs-green-energy.jpg",
-      position: "-650px", /*changes the horizontal offset of the image, positive value pans image to left, negative pans to right*/
+      position:
+        "-650px" /*changes the horizontal offset of the image, positive value pans image to left, negative pans to right*/,
       btnIndex: 0,
       btnRoute: "/all-about-green-energy/learn-more",
     },
@@ -33,7 +34,8 @@ const Home = () => {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       ],
       img: "https://media.npr.org/assets/img/2014/03/18/464593903-polar-bears-water_wide-020cc7e692251ae0a9dd350208ddf43bf5a764d0-s1400-c100.jpg",
-      position: "-60px", /*changes the horizontal offset of the image, positive value pans image to left, negative pans to right*/
+      position:
+        "-60px" /*changes the horizontal offset of the image, positive value pans image to left, negative pans to right*/,
       btnIndex: 0,
       btnRoute: "/all-about-green-energy/climate-change",
     },
@@ -43,7 +45,8 @@ const Home = () => {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       ],
       img: "https://kellogggarden.com/wp-content/uploads/2020/05/family-planting-tree.jpg",
-      position: "-140px", /*changes the horizontal offset of the image, positive value pans image to left, negative pans to right*/
+      position:
+        "-140px" /*changes the horizontal offset of the image, positive value pans image to left, negative pans to right*/,
       btnIndex: 0,
       btnRoute: "/all-about-green-energy/living-green",
     },
@@ -66,7 +69,7 @@ const Home = () => {
   return (
     <div>
       <div className="video-container">
-        <video src={videoBg} autoPlay loop muted />
+        <video src={videoBg} autoPlay loop muted playsInline />
         <button
           className="home-learn-more-btn"
           onClick={toLearnMoreHandler}
@@ -101,12 +104,20 @@ const Home = () => {
             <Fade bottom>
               <div className="content-container">
                 {isEven(index) ? (
-                  <img src={item.img} className="half" style={{objectPosition:item.position}} />
+                  <img
+                    src={item.img}
+                    className="half"
+                    style={{ objectPosition: item.position }}
+                  />
                 ) : (
                   <HomeText />
                 )}
                 {!isEven(index) ? (
-                  <img src={item.img} className="half" style={{objectPosition:item.position}}/>
+                  <img
+                    src={item.img}
+                    className="half"
+                    style={{ objectPosition: item.position }}
+                  />
                 ) : (
                   <HomeText />
                 )}
